d7bcd06606676ec9c0232fd4080ba1fb3a9077e6
dmitryghoncharko <dafae722@gmail.com>
2024-09-14 13:07:31 +0300

    инициализация проекта

---
+++ src/main/java/by/ghoncharko/interview_training_platform/InterviewTrainingPlatformApplication.java
@@ -0,0 +1,13 @@
+package by.ghoncharko.interview_training_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewTrainingPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewTrainingPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/ghoncharko/interview_training_platform/InterviewTrainingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package by.ghoncharko.interview_training_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewTrainingPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
